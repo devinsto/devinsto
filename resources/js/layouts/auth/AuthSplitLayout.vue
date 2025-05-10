8d908769a8f8a8ebfda47ba850b0afdbd4ce7a44
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-10 09:30:03 +0100

    login left  and design right

--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -15,6 +15,18 @@ defineProps<{
 <template>
     <div
         class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div class="lg:p-8">
+            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div class="mb-4 ml-30 flex h-9 w-9 items-center justify-center rounded-md">
+                        <AppLogoIcon class="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                    </div>
+                <div class="flex flex-col space-y-2 text-center">
+                    <h1 class="text-xl font-medium tracking-tight" v-if="title">{{ title }}</h1>
+                    <p class="text-sm text-muted-foreground" v-if="description">{{ description }}</p>
+                </div>
+                <slot />
+            </div>
+        </div>
         <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
             <div class="absolute inset-0 bg-red-900" id="left-s"/>
             <Link :href="route('home')" class="relative z-20 flex items-center text-lg font-medium">
@@ -28,18 +40,7 @@ defineProps<{
                 </blockquote>
             </div>
         </div>
-        <div class="lg:p-8">
-            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                <div class="mb-4 ml-30 flex h-9 w-9 items-center justify-center rounded-md">
-                        <AppLogoIcon class="size-9 fill-current text-[var(--foreground)] dark:text-white" />
-                    </div>
-                <div class="flex flex-col space-y-2 text-center">
-                    <h1 class="text-xl font-medium tracking-tight" v-if="title">{{ title }}</h1>
-                    <p class="text-sm text-muted-foreground" v-if="description">{{ description }}</p>
-                </div>
-                <slot />
-            </div>
-        </div>
+       
     </div>
 </template>
 
