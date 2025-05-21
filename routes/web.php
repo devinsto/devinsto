f54231a9495daf77539dde2df3b71c9d7318ce01
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-21 13:23:18 +0100

    sdd

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
